a33fde1202a789d490d7c90fd27a71c00f3a31e3
kfwong <kfwong@zoho.com>
2018-09-12 23:16:32 +0800

    Added semantic ui. Basic draft.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Container, Grid, Image, Header, Form } from 'semantic-ui-react';
+import backgroundImage from './assets/bt-register-background.png';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="fullbody">
+        <Grid verticalAlign='middle' style={{ height: '100%' }} centered container>
+          <Grid.Column>
+            <Form size='huge'>
+              <Form.Field>
+                <Header className='logo' textAlign='center'>
+                  BACON IPSUM
+                  <Header.Subheader className='logo-subheader'>Bacon ipsum dolor amet shank andouille jowl pastrami.</Header.Subheader>
+                </Header>
+              </Form.Field>
+              <Form.Input placeholder='Email Address' />
+              <Form.Input placeholder='Password' type='password'/>
+              <Form.Button size='huge' fluid>SIGN IN</Form.Button>
+            </Form>
+          </Grid.Column>
+        </Grid>
       </div>
     );
   }
