f0fcc11f3bb2ccfd0cc2e693998034a726c1faa1
kfwong <kfwong@zoho.com>
2018-09-14 21:45:22 +0800

    Added item detail.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './Home'
 import Profile from './Profile'
 import CountryListing from './CountryListing'
 import ItemListing from './ItemListing'
+import ItemDetail from './ItemDetail';
 
 const Sidebar = () => (
     <Router>
@@ -25,6 +26,9 @@ const Sidebar = () => (
                 <li>
                     <Link to="/item-listing">Item Listing</Link>
                 </li>
+                <li>
+                    <Link to="/item-detail">Item Detail</Link>
+                </li>
             </ul>
             <hr />
             <Route path="/" component={Home} />
@@ -32,6 +36,7 @@ const Sidebar = () => (
             <Route path="/profile" component={Profile} />
             <Route path="/country-listing" component={CountryListing} />
             <Route path="/item-listing" component={ItemListing} />
+            <Route path="/item-detail" component={ItemDetail} />
         </div>
     </Router>
 )
