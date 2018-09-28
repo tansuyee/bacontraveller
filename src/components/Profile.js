8895202dbd7ce0fe65d22e521891bb8549919f00
kfwong <kfwong@zoho.com>
2018-09-28 18:00:44 +0800

    fix default user image.

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -3,10 +3,11 @@ import React, { Component } from 'react';
 import * as actions from '../actions';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { Grid, Header, Card, Image, Statistic, Item, Loader, Button, Icon, Divider } from 'semantic-ui-react';
+import { Grid, Header, Card, Image, Statistic, Item, Loader, Button } from 'semantic-ui-react';
 import styles from '../static/css/Profile.module.css';
 import moment from 'moment';
 import { getCountryName } from '../helper';
+import {defaultImageUrl} from '../constant'
 
 class Profile extends Component {
 
@@ -31,21 +32,21 @@ class Profile extends Component {
 
   renderItem(item, isRequest) {
     if (!isRequest) item = item.Post;
-    return (
+    return(
       <Item key={item.id}>
         <Item.Image size='tiny' src={item.item_image_url} />
 
         <Item.Content verticalAlign='middle'>
           <Item.Header as={Link} to={`item-detail/${item.id}`}>{item.item_name}</Item.Header>
           <Item.Meta>From {getCountryName(item.country_from)}</Item.Meta>
-          {(isRequest && item.transactions.length !== 0) &&
+          { (isRequest && item.transactions.length !== 0) &&
             <Item.Description>
               {item.transactions.length} transactions
             </Item.Description>
           }
           <Item.Extra>Status: {item.status}</Item.Extra>
           <Item.Extra>{moment(item.createdAt).fromNow()}</Item.Extra>
-          {isRequest &&
+          { isRequest &&
             <Item.Extra>
               <Button className={styles.deletePost} floated='right' size='mini'
                 onClick={() => this.props.deletePost(item.id)}
@@ -53,7 +54,7 @@ class Profile extends Component {
               <Button className={styles.editPost} floated='right' size='mini'
                 onClick={() => this.props.history.push(`/post-edit/${item.id}`)}
               >Edit</Button>
-              {item.status === "ACCEPTED" &&
+              { item.status === "ACCEPTED" &&
                 <Button className={styles.completePost} floated='right' size='mini'
                   onClick={() => this.props.completePost(item.id)}
                 >Mark as Completed</Button>
@@ -68,27 +69,27 @@ class Profile extends Component {
   renderRequestsOrOfferings(list, isRequest) {
     return (
       <Grid container>
-        <Grid.Row>
-          <Grid.Column>
-            <Header className={styles.plannedTripTitle}>
-              {isRequest ? 'Requests' : 'Offerings'} ({list.length})
-              </Header>
-            {!list.length &&
-              <p className={styles.titleSubheader}>There is no {isRequest ? 'requests' : 'offerings'}.</p>
-            }
-          </Grid.Column>
-        </Grid.Row>
-        {list.length !== 0 &&
           <Grid.Row>
             <Grid.Column>
-              <Item.Group className={styles.plannedTripContent} unstackable divided>
-                {_.orderBy(list, 'createdAt', 'desc').map((item) => {
-                  return this.renderItem(item, isRequest);
-                })}
-              </Item.Group>
+              <Header className={styles.plannedTripTitle}>
+                { isRequest? 'Requests' : 'Offerings' } ({list.length})
+              </Header>
+              { !list.length &&
+                <p className={styles.titleSubheader}>There is no { isRequest? 'requests' : 'offerings' }.</p>
+              }
             </Grid.Column>
           </Grid.Row>
-        }
+          { list.length !== 0 &&
+            <Grid.Row>
+              <Grid.Column>
+                <Item.Group className={styles.plannedTripContent} unstackable divided>
+                  {_.orderBy(list, 'createdAt', 'desc').map((item) => {
+                    return this.renderItem(item, isRequest);
+                  })}
+                </Item.Group>
+              </Grid.Column>
+            </Grid.Row>
+          }
       </Grid>
     );
   }
@@ -98,11 +99,15 @@ class Profile extends Component {
     if (!this.props.auth.isLoggedIn) {
       return (
         <div className={styles.notLoggedIn}>
-          <div>
-            <Icon className={styles.lock} name='lock' />
-            <h3 className={styles.title}>You are not logged in.</h3>
-            <Button className={styles.login} onClick={() => this.props.history.push('/register')}>LOGIN</Button>
-          </div>
+          <Grid container>
+            <Grid.Row>
+              <Grid.Column>
+                <Header className={styles.title} as='h1'>
+                  <span>You are not logged in.</span>
+                </Header>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
         </div>
       )
     }
@@ -122,9 +127,9 @@ class Profile extends Component {
     }
 
     const userStats = [
-      { key: 'dealClosed', label: 'Deal Closed', value: currUser.deals_closed_count },
-      { key: 'followers', label: 'Followers', value: currUser.followers_count },
-      { key: 'following', label: 'Following', value: currUser.following.length },
+        { key: 'dealClosed', label: 'Deal Closed', value: currUser.deals_closed_count },
+        { key: 'followers', label: 'Followers', value: currUser.followers_count },
+        { key: 'following', label: 'Following', value: currUser.following.length },
     ]
     return (
       <div className={styles.container}>
@@ -143,15 +148,15 @@ class Profile extends Component {
             <Grid.Column>
               <Card>
                 <Card.Content>
-                  {currUser.image_url ?
-                    <Image floated='left' size='tiny' src={currUser.image_url} /> :
-                    <Image floated='left' size='tiny' src='https://randomuser.me/api/portraits/women/8.jpg' />
-                  }
-                  <Card.Header>
-                    {currUser.username}
-                    <Header.Subheader className={styles.rank}>
-                      Joined {moment(currUser.createdAt).format("MMMM Do YYYY")}
-                    </Header.Subheader>
+                 { currUser.image_url ?
+                   <Image floated='left' size='tiny' src={currUser.image_url} /> :
+                   <Image floated='left' size='tiny' src={defaultImageUrl} />
+                 }
+                <Card.Header>
+                  {currUser.username}
+                  <Header.Subheader className={styles.rank}>
+                    Joined {moment(currUser.createdAt).format("MMMM Do YYYY")}
+                  </Header.Subheader>
                   </Card.Header>
                   <Card.Meta className={styles.bio}>
                     Take only memories, leave only footprints.
